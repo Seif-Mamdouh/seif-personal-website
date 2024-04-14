75d857dfad59b5a42351f05fcc10c8a94e139d01
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-04-14 14:23:55 -0400

    nav

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -2,19 +2,18 @@
 import Link from 'next/link';
 import React from 'react'
 
-
 const Nav = () => {
     return (
       <nav className="nav">
         <ul>
           <li>
-              <Link href="/experience">Experience</Link>
-          </li>
-          <li>
-              <Link href="/reachOut">Contact</Link>
+            <Link href="/experience">Experience</Link>
           </li>
           <li>
             <Link href="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
           </li>
         </ul>
       </nav>
@@ -22,3 +21,7 @@ const Nav = () => {
 }
 
 export default Nav
+
+
+
+
