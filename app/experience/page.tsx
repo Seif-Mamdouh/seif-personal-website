4a14c4bc79fd11d5bef4597a871d06e72787391c
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-06-20 08:28:45 -0400

    changing images and font

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -12,7 +12,9 @@ const Experience = () => {
       <div className="experience-item">
         <div className="experience-role" >
           <img src="/Ernst.png" alt="Ernst & Young Logo" className="experience-logo" width={100} height={100} style={{marginRight: '3%'}}/>
-          Ernst & Young | Software Engineer Intern June 2023 - Aug 2023
+          Ernst & Young | Software Engineer Intern
+          <br/>
+          June 2023 - Aug 2023
         </div>
         <div className="experience-description">
         Automated over 200 test cases using Tricentis Tosca, leading to a 40% reduction in testing time and enhanced software quality.
@@ -23,7 +25,9 @@ const Experience = () => {
       <div className="experience-item">
         <div className="experience-role">
           <img src="/cfa.png" alt="CFA Logo" className="experience-logo" width={90} height={90} style={{marginRight: '3%'}}/>
-          Chick-Fil-A Corporate | Software Engineer Intern Jan 2023 - May 2023
+          Chick-Fil-A Corporate | Software Engineer Intern
+          <br/>
+          Jan 2023 - May 2023
         </div>
         <div className="experience-description">
         Led the end-to-end development of the Location Viewer app in ReactJS within 6 weeks, achieving 1300 active users.
@@ -34,17 +38,21 @@ const Experience = () => {
       <div className="experience-item">
         <div className="experience-role">
           <img src="/njt.png" alt="NJT Logo" className="experience-logo" width={90} height={90} style={{marginRight: '3%'}}/>
-          NJ Transit | Software Engineer Intern June 2022 - Aug 2022
+          NJ Transit | Software Engineer Intern
+          <br/>
+          June 2022 - Aug 2022
         </div>
         <div className="experience-description">
-          Reduced data processing time from 3 minutes to 3 minutes by implementing an automated system in Python</div>
+          Reduced data processing time from 3 minutes to 30 seconds by implementing an automated system in Python</div>
       </div>
 
       {/* Wi-Drive */}
       <div className="experience-item">
         <div className="experience-role">
-          <img src="/wd.png" alt="Wi-Drive Logo" className="experience-logo" width={70} height={70} style={{marginRight: '5%'}}/>
-          Wi-Drive | Software Engineer Intern Aug 2021 - Dec 2021
+          <img src="/wd.png" alt="Wi-Drive Logo" className="experience-logo" width={60} height={60} style={{marginRight: '5%'}}/>
+          Wi-Drive | Software Engineer Intern
+          <br/>
+          Aug 2021 - Dec 2021
         </div>
         <div className="experience-description">
           Enhanced user engagement and reservation funnel by 35% by developing a new engaging website.
@@ -55,7 +63,9 @@ const Experience = () => {
       <div className="experience-item">
         <div className="experience-role">
           <img src="/kl.png" alt="Kanopy Labs Logo" className="experience-logo" width={100} height={100} style={{marginRight: '3%'}}/>
-          Kanopy Labs | Software Engineer Intern May 2021 - June 2021
+          Kanopy Labs | Software Engineer Intern
+          <br/>
+          May 2021 - June 2021
         </div>
         <div className="experience-description">
           Engaged with clients and provide consultation on software solutions, business plans and design to bring client's ideas to life.
