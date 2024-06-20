63afa8ccdc065b7a602cc7f20f692087dbc5e8e1
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-06-20 09:27:46 -0400

    small changes

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -17,7 +17,7 @@ const Experience = () => {
           June 2023 - Aug 2023
         </div>
         <div className="experience-description">
-        Automated over 200 test cases using Tricentis Tosca, leading to a 40% reduction in testing time and enhanced software quality.
+        Automated over <strong>200 test cases</strong> using Tricentis Tosca, leading to a <strong>40% reduction</strong> in testing time and enhanced software quality.
         </div>
       </div>
 
@@ -25,12 +25,12 @@ const Experience = () => {
       <div className="experience-item">
         <div className="experience-role">
           <img src="/cfa.png" alt="CFA Logo" className="experience-logo" width={90} height={90} style={{marginRight: '3%'}}/>
-          Chick-Fil-A Corporate | Software Engineer Intern
+          Chick-Fil-A | Software Engineer Intern
           <br/>
           Jan 2023 - May 2023
         </div>
         <div className="experience-description">
-        Led the end-to-end development of the Location Viewer app in ReactJS within 6 weeks, achieving 1300 active users.
+        Led the end-to-end development of the Location Viewer app in ReactJS <strong>within 6 weeks</strong>, achieving <strong>1300 active users</strong>.
         </div>
       </div>
 
@@ -38,24 +38,24 @@ const Experience = () => {
       <div className="experience-item">
         <div className="experience-role">
           <img src="/njt.png" alt="NJT Logo" className="experience-logo" width={90} height={90} style={{marginRight: '3%'}}/>
-          NJ Transit | Software Engineer Intern
+          New Jeresy Transit | Software Engineer Intern
           <br/>
           June 2022 - Aug 2022
         </div>
         <div className="experience-description">
-          Reduced data processing time from 3 minutes to 30 seconds by implementing an automated system in Python</div>
+          Reduced data processing time from <strong>3 minutes</strong> to <strong>30 seconds</strong> by implementing an automated system in Python</div>
       </div>
 
       {/* Wi-Drive */}
       <div className="experience-item">
         <div className="experience-role">
-          <img src="/wd.png" alt="Wi-Drive Logo" className="experience-logo" width={60} height={60} style={{marginRight: '5%'}}/>
+          <img src="/wt.png" alt="Wi-Drive Logo" className="experience-logo" style={{marginRight: '5%'}}/>
           Wi-Drive | Software Engineer Intern
           <br/>
           Aug 2021 - Dec 2021
         </div>
         <div className="experience-description">
-          Enhanced user engagement and reservation funnel by 35% by developing a new engaging website.
+          Enhanced <strong>user engagement</strong> and <strong>reservation funnel</strong> by <strong>35%</strong> by developing a new engaging website.
         </div>
       </div>
 
